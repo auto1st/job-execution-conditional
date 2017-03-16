ee66c08a03064a74f18e2675b50c8c27496b3eef
fabiojose <fabiojose@gmail.com>
2017-03-16 17:50:13 -0300

    fix identation

--- src/main/java/com/github/auto1st/rundeck/plugin/JobExecutionConditional.java
+++ src/main/java/com/github/auto1st/rundeck/plugin/JobExecutionConditional.java
@@ -50,154 +50,148 @@ import grails.util.Holders;
 @Plugin(name = JobExecutionConditional.PROVIDER_NAME, service = ServiceNameConstants.WorkflowStep)
 @PluginDescription(title = "Job Execution Conditional", description = "Evaluate executions options of another Job")
 public class JobExecutionConditional implements StepPlugin {
-  
-  private static final String SESSION_FACTORY  = "sessionFactory";
+
+  private static final String SESSION_FACTORY = "sessionFactory";
   private static final String EXECUTIONS_QUERY_ALL = "from Execution e where e.scheduledExecution.uuid = :uuid";
   private static final String EXECUTIONS_QUERY_ALL_PARAM0 = "uuid";
-  
-  private static final String METHOD_GET_ARGSTRING = "getArgString";
-		
-	public static final String PROVIDER_NAME = "job-execution-conditional";
-	
-	@PluginProperty(title = "Job UUID",
-	                description = "UUID for the Job", 
-	                required = true)
-	String jobUUID;
-	
-	@PluginProperty(title = "Maximum executions", 
-	                description = "Maximum number of latest executions to analyze", 
-	                defaultValue = "0")
-	Integer maximumExecutions;
-	
-	@PluginProperty(title = "Analyze failed", 
-	                description = "Analyze the execution, even if it was failed")
-	Boolean analyzeFailed;
-	
-	
-	@PluginProperty(title = "Option condition",
-			            description = "Use options values to get a true comparison"
-	                              + "\n"
-			                          + "* One option per line"
-			                          + "\n"
-			                          + "Example:"
-			                          + "\n"
-			                          + "`-OPTION_NAME_A=${option.OPTION_0} -OPTION_NAME_B=${option.OPTION_1}`",
-			            validatorClass = OptionConditionValidator.class,
-			            required = true)
-	@TextArea
-	String optionCondition;
-	
-	private static enum Failures implements FailureReason {
-	  GeneralFailure
-	}
 
-	@Override
-	public void executeStep(final PluginStepContext context, final Map<String, Object> map) throws StepException {
-	  
-	  /*
-	   * Get the job service from context.
-	   */
-	  final JobService jobService = context.getExecutionContext().getJobService();
-	  
-	  /*
-	   * Extract options from optionCondition
-	   */
-	  final Map<String, String> _conditions = Utils.conditionsOf(optionCondition);
-	  final Set<String> _conditionsKeys     = _conditions.keySet();
-	  
-	  /*
-	   * Hack to get the hibernate session from Grails APP
-	   */
-	  final GrailsApplication grails = Holders.getGrailsApplication();
+  private static final String METHOD_GET_ARGSTRING = "getArgString";
+
+  public static final String PROVIDER_NAME = "job-execution-conditional";
+
+  @PluginProperty(title = "Job UUID",
+                  description = "UUID for the Job", 
+                  required = true)
+  String jobUUID;
+
+  @PluginProperty(title = "Maximum executions", 
+                  description = "Maximum number of latest executions to analyze", 
+                  defaultValue = "0")
+  Integer maximumExecutions;
+
+  @PluginProperty(title = "Analyze failed", 
+                  description = "Analyze the execution, even if it was failed")
+  Boolean analyzeFailed;
+
+  @PluginProperty(title = "Option condition", 
+                  description = "Use options values to get a true comparison" 
+                                + "\n"
+                                + "* One option per line" + "\n" + "Example:" + "\n"
+                                + "`-OPTION_NAME_A=${option.OPTION_0} -OPTION_NAME_B=${option.OPTION_1}`",
+                  validatorClass = OptionConditionValidator.class, 
+                  required = true)
+  @TextArea
+  String optionCondition;
+
+  private static enum Failures implements FailureReason {
+    GeneralFailure
+  }
+
+  @Override
+  public void executeStep(final PluginStepContext context, final Map<String, Object> map) throws StepException {
+
+    /*
+     * Get the job service from context.
+     */
+    final JobService jobService = context.getExecutionContext().getJobService();
+
+    /*
+     * Extract options from optionCondition
+     */
+    final Map<String, String> _conditions = Utils.conditionsOf(optionCondition);
+    final Set<String> _conditionsKeys = _conditions.keySet();
+
+    /*
+     * Hack to get the hibernate session from Grails APP
+     */
+    final GrailsApplication grails = Holders.getGrailsApplication();
     final ApplicationContext mainContext = grails.getMainContext();
-	  final SessionFactory factory = (SessionFactory)mainContext.getBean(SESSION_FACTORY);
+    final SessionFactory factory = (SessionFactory) mainContext.getBean(SESSION_FACTORY);
     Session session = null;
-    
-	  try{
-	    /*
-	     * Get the job reference by UUID
-	     */
-	    final JobReference job = jobService.jobForID(jobUUID, context.getFrameworkProject());
-	    
-	    /*
-	     * Start a new hibernate session.
-	     */
-	    session = factory.openSession();
-	 
-	    /*
-	     * Execute the query.
-	     */
-	    final Query query = session.createQuery(EXECUTIONS_QUERY_ALL);
-	    query.setParameter(EXECUTIONS_QUERY_ALL_PARAM0, job.getId());
-	    
-	    //execute the query
-	    final List<?> _executions = query.list();
-	    
-	    /*
+
+    try {
+      /*
+       * Get the job reference by UUID
+       */
+      final JobReference job = jobService.jobForID(jobUUID, context.getFrameworkProject());
+
+      /*
+       * Start a new hibernate session.
+       */
+      session = factory.openSession();
+
+      /*
+       * Execute the query.
+       */
+      final Query query = session.createQuery(EXECUTIONS_QUERY_ALL);
+      query.setParameter(EXECUTIONS_QUERY_ALL_PARAM0, job.getId());
+
+      // execute the query
+      final List<?> _executions = query.list();
+
+      /*
        * At then end if it has true, conditions are met
        */
       boolean _continue = _executions.size() > 0;
-      
+
       /*
        * For each execution.
        */
-	    for(int _index = 0; _index < _executions.size(); _index++){
-	      Object _execution = _executions.get(_index);
-	      
-	      //local boolean accumulator
-	      boolean _local = true;
-	      
-	      /*
-	       * Get the argString value. The options list used by the execution.
-	       */
-	      Object _argStringObj = MethodUtils.invokeMethod(_execution, METHOD_GET_ARGSTRING);
-	      
-	      if(_argStringObj instanceof String){
-	        
-	        /*
-	         * Extract the pairs from argString used by the Execution
-	         */
-	        Map<String, String> _options = Utils.optionsOf((String)_argStringObj);
-	        
-	        /*
-	         * For each condition
-	         */
-	        for(String _key:_conditionsKeys){
-	          //compare the condition value to execution option value
-	          _local = _local &&
-	                   _conditions.get(_key).equals(_options.get(_key));
-	        }
-	      }
-	      
-	      //compare holder
-	      _continue = _local;
-	      
-	      /*
+      for (int _index = 0; _index < _executions.size(); _index++) {
+        Object _execution = _executions.get(_index);
+
+        // local boolean accumulator
+        boolean _local = true;
+
+        /*
+         * Get the argString value. The options list used by the execution.
+         */
+        Object _argStringObj = MethodUtils.invokeMethod(_execution, METHOD_GET_ARGSTRING);
+
+        if (_argStringObj instanceof String) {
+
+          /*
+           * Extract the pairs from argString used by the Execution
+           */
+          Map<String, String> _options = Utils.optionsOf((String) _argStringObj);
+
+          /*
+           * For each condition
+           */
+          for (String _key : _conditionsKeys) {
+            // compare the condition value to execution option value
+            _local = _local && _conditions.get(_key).equals(_options.get(_key));
+          }
+        }
+
+        // compare holder
+        _continue = _local;
+
+        /*
          * When the first true compare is found, breaks the loop.
          */
-        if(_local){
+        if (_local) {
           break;
         }
-	    }
-	    
-	    if(_continue){
-	      //conditions are met
-	      context.getLogger().log(2, "Conditions are met: " + _conditions);
-	    }else{
-	      //conditions aren't met
-	      context.getLogger().log(0, "Conditions aren't met: " + _conditions);
-	      context.getFlowControl().Halt("No true comparison found");
-	    }
-	     
-	  }catch(Exception e){
-	    throw new StepException(e.getMessage(), e, Failures.GeneralFailure);
-	  }finally{
-	    if(null!= session){
-	      //close the hibernate session
-	      session.close();
-	    }
-	  }
-	}
+      }
+
+      if (_continue) {
+        // conditions are met
+        context.getLogger().log(2, "Conditions are met: " + _conditions);
+      } else {
+        // conditions aren't met
+        context.getLogger().log(0, "Conditions aren't met: " + _conditions);
+        context.getFlowControl().Halt("No true comparison found");
+      }
+
+    } catch (Exception e) {
+      throw new StepException(e.getMessage(), e, Failures.GeneralFailure);
+    } finally {
+      if (null != session) {
+        // close the hibernate session
+        session.close();
+      }
+    }
+  }
 
 }
-
--- src/main/java/com/github/auto1st/rundeck/plugin/Utils.java
+++ src/main/java/com/github/auto1st/rundeck/plugin/Utils.java
@@ -29,121 +29,135 @@ import java.util.regex.Pattern;
  *
  */
 public final class Utils {
-	
-	protected static final Properties PROPERTIES = new Properties();
-	static {
-		try{
-			PROPERTIES.load(JobExecutionConditional.class.getResourceAsStream("/resources/plugin.properties"));
-		}catch(IOException e){
-			throw new RuntimeException(e.getMessage(), e);
-		}
-	};
 
-	private Utils() {
-		
-	}
-	
-	/**
-   * Extract the pairs from <code>-VAR=${option.VAR3}</code>, where <code>"VAR"</code> is the key and <code>"${option.VAR3}"</code> is the value
-   * @param value The text with one or more pairs
+  /**
+   * Plugin properties loaded from /resource/plugin.properties
+   */
+  protected static final Properties PROPERTIES = new Properties();
+  static {
+    try {
+      PROPERTIES.load(JobExecutionConditional.class.getResourceAsStream("/resources/plugin.properties"));
+    } catch (IOException e) {
+      throw new RuntimeException(e.getMessage(), e);
+    }
+  };
+
+  private Utils() {
+
+  }
+
+  /**
+   * Extract the pairs from <code>-VAR=${option.VAR3}</code>, where
+   * <code>"VAR"</code> is the key and <code>"${option.VAR3}"</code> is the
+   * value
+   * 
+   * @param value
+   *          The text with one or more pairs
    * @return <code>java.util.Map</code>
    */
-	public static Map<String, String> conditionsOf(final String value) {
+  public static Map<String, String> conditionsOf(final String value) {
     final Map<String, String> _result = new HashMap<String, String>();
-    
+
     final Pattern pattern = Pattern.compile(Utils.PROPERTIES.getProperty("regex.option"));
     final Matcher matcher = pattern.matcher(value);
-    while(matcher.find()){
+    while (matcher.find()) {
       String _key = matcher.group(1);
-      
-      //remove the hyphen symbol
+
+      // remove the hyphen symbol
       _key = _key.substring(1);
-      
-      //remove the equals symbol
+
+      // remove the equals symbol
       _key = _key.substring(0, _key.length() - 1);
-      
-      //put the _key and the value in the map
+
+      // put the _key and the value in the map
       _result.put(_key, matcher.group(2));
     }
-    
+
     return _result;
   }
-	
-	/**
-	 * Extract the pairs from <code>-VAR0 value -VAR1 value</code>, where <code>"VAR0"</code> is the key and <code>"value"</code> is the value.
-	 * @param text The text with one or more pairs
-	 * @return <code>java.util.Map</code>
-	 */
-	public static Map<String, String> optionsOf(final String text) {
-	  final Map<String, String> _result = new HashMap<String, String>();
-	  
-	  final Pattern pattern = Pattern.compile(Utils.PROPERTIES.getProperty("regex.args"));
-	  final Matcher matcher = pattern.matcher(text);
-	  
-	  while(matcher.find()){
-	    
-	    /*
-	     * Part one: execution key
-	     */
-	    String _key = matcher.group(1);
-	    
-      //remove the hyphen and get VAR0
+
+  /**
+   * Extract the pairs from <code>-VAR0 value -VAR1 value</code>, where
+   * <code>"VAR0"</code> is the key and <code>"value"</code> is the value.
+   * 
+   * @param text
+   *          The text with one or more pairs
+   * @return <code>java.util.Map</code>
+   */
+  public static Map<String, String> optionsOf(final String text) {
+    final Map<String, String> _result = new HashMap<String, String>();
+
+    final Pattern pattern = Pattern.compile(Utils.PROPERTIES.getProperty("regex.args"));
+    final Matcher matcher = pattern.matcher(text);
+
+    while (matcher.find()) {
+
+      /*
+       * Part one: execution key
+       */
+      String _key = matcher.group(1);
+
+      // remove the hyphen and get VAR0
       _key = _key.substring(1);
-      
-      //remove spaces
+
+      // remove spaces
       _key = _key.trim();
-      
+
       /*
        * Part two: execution value
        */
       String _value = matcher.group(2);
-      
+
       _result.put(_key, _value);
-	  }
-	  
-	  return _result;
-	}
-	
-	/**
-	 * Expand the expression equals this <code>${option.VAR3}</code>, getting the value from data context.<br/>
-	 * 
-	 * @param expr Expression
-	 * @param dataContext Rundeck data context
-	 * @return Value found in data context or <code>null</code>
-	 */
-	public static String expand(final String expr, final Map<String, Map<String, String>> dataContext) {
-		
-		final Pattern pattern = Pattern.compile(Utils.PROPERTIES.getProperty("regex.option.group"));
-		final Matcher matcher = pattern.matcher(expr);
-		
-		matcher.find();
-		if(matcher.groupCount() >= 1){
-			String group = matcher.group(1);
-			
-			if(group.startsWith("option.")){
-				
-				String var = group.split("\\.")[1];
-				Map<String, String> _options = dataContext.get("option");
-				if(null!= _options && _options.containsKey(var)){
-					return _options.get(var);
-				}
-				
-			}
-		}
-		
-		return null;
-	}
-	
-	/**
-	 * Check if a value is an expression like this <code>${option.VAR3}</code>
-	 * @param value
-	 * @return <code>true</code> if value is an expression
-	 */
-	public static boolean isExpr(final String value) {
-	  
-	  final Pattern pattern = Pattern.compile(Utils.PROPERTIES.getProperty("regex.option.group"));
-	  final Matcher matcher = pattern.matcher(value);
-	  
-	  return matcher.matches();
-	}
+    }
+
+    return _result;
+  }
+
+  /**
+   * Expand the expression equals this <code>${option.VAR3}</code>, getting the
+   * value from data context.<br/>
+   * 
+   * @param expr
+   *          Expression
+   * @param dataContext
+   *          Rundeck data context
+   * @return Value found in data context or <code>null</code>
+   */
+  public static String expand(final String expr, final Map<String, Map<String, String>> dataContext) {
+
+    final Pattern pattern = Pattern.compile(Utils.PROPERTIES.getProperty("regex.option.group"));
+    final Matcher matcher = pattern.matcher(expr);
+
+    matcher.find();
+    if (matcher.groupCount() >= 1) {
+      String group = matcher.group(1);
+
+      if (group.startsWith("option.")) {
+
+        String var = group.split("\\.")[1];
+        Map<String, String> _options = dataContext.get("option");
+        if (null != _options && _options.containsKey(var)) {
+          return _options.get(var);
+        }
+
+      }
+    }
+
+    return null;
+  }
+
+  /**
+   * Check if a value is an expression like this <code>${option.VAR3}</code>
+   * 
+   * @param value
+   * @return <code>true</code> if value is an expression
+   */
+  public static boolean isExpr(final String value) {
+
+    final Pattern pattern = Pattern.compile(Utils.PROPERTIES.getProperty("regex.option.group"));
+    final Matcher matcher = pattern.matcher(value);
+
+    return matcher.matches();
+  }
 }
